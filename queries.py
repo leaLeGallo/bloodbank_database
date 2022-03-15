aea0d9de9b848bdfac18e6906cfb1a4e33ea423d
leaLeGallo <legallolea@outlook.fr>
2022-03-15 19:33:32 +0100

    trying queries

--- queries.py
+++ queries.py
@@ -1,8 +1,8 @@
 #All queries for the bloodbank application
 
-def create_stocks_view(cursor):
-    query = "create view availableStocks as select bloodType, sum(donations.quantity) as stock from donors"\
-            "join donations on donors.donorID = donations.donorID "\
-            "where donations.donationsID not in (select donationsID from transfusions)"\
-            "group by bloodtype"\
-            "order by stock desc"
+
+def show_table(table):
+    query = f"select * from {table}"
+    return query
+
+
