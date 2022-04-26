1d54e8978a22d84261704f13b475c78649569531
leaLeGallo <legallolea@outlook.fr>
2022-04-26 12:55:45 +0200

    structured the code, added comments

--- GUI.py
+++ GUI.py
@@ -1,3 +1,5 @@
+# The application's interface.
+
 from bloodBank import *
 import queries as q
 from tkinter.ttk import *
@@ -14,12 +16,13 @@ cursor = cnx.cursor(buffered=True)
 try:
     cursor.execute("USE {}".format(DATABASE_NAME)) # function to use database
 except mysql.connector.Error as err:
-    print("Database {} does not exist".format(DATABASE_NAME)) # if it does not exist
+    print("Database {} does not exist".format(DATABASE_NAME)) 
     if err.errno == errorcode.ER_BAD_DB_ERROR:
+        # creating and popuating the database if it does not exist
         creating_databases(cursor, DATABASE_NAME)
-        print("Database {} created succesfully.".format(DATABASE_NAME)) # if created
+        print("Database {} created succesfully.".format(DATABASE_NAME)) 
         cnx.database = DATABASE_NAME
-        create_tables_donors(cursor) # calling functions
+        create_tables_donors(cursor) 
         insert_into_donors(cursor)
         create_tables_recipients(cursor)
         insert_into_recipients(cursor)
@@ -40,16 +43,18 @@ else:
 def retrieveTables():
     try:
         table = combo.get()
-        newWindow = tk.Tk()
+        newWindow = tk.Tk() # create new window for the table
         newWindow.title(f"{table}")
         q.show_table(table, cursor)
         headers = [i[0] for i in cursor.description]
         i = 0
+        # show the column names
         for header in headers:
             e = Label(newWindow,width=17, text=header, borderwidth=2,relief='ridge', anchor="w")
             e.grid(row=0, column=i)
             i+=1
         i = 1
+        # show the rows
         for row in cursor: 
             for j in range(len(row)):
                 e = Label(newWindow,width=17, text=row[j], borderwidth=2,relief='ridge', anchor="w")  
@@ -88,7 +93,7 @@ def retrieveNextDonation(fname, lname):
     try:
         wholename = fname + " " + lname
         donationWindow = Toplevel(window) # new window for donation
-        donationWindow.title(wholename) # title 
+        donationWindow.title(wholename)
         nextdonlabel = Label( donationWindow, text = q.nextdonation(wholename, cursor) ) # call nextDonation
         donationWindow.geometry('300x100')
         nextdonlabel.grid(column=0, row=0)
@@ -99,7 +104,7 @@ def retrieveNextDonation(fname, lname):
 def retrieveGiving(fname, lname):
     try:
         wholename = fname + " " + lname
-        givingWindow = Toplevel(window)
+        givingWindow = Toplevel(window) # creates window for the info
         givingWindow.title(wholename)
         lbl = Label( givingWindow,
         text = q.givingblood(wholename, cursor) )
@@ -112,10 +117,10 @@ def retrieveGiving(fname, lname):
 def add_data():
     try:
         table = insert.get()
-        r = Toplevel(window)
+        r = Toplevel(window) # new window showing the modifiable table
         tree = ttk.Treeview(r)
         q.show_table(table, cursor)
-        headers = [i[0] for i in cursor.description]
+        headers = [i[0] for i in cursor.description] # retrieve headers
         tree['show'] = 'headings'
         
         r.geometry("1050x700")
@@ -129,10 +134,11 @@ def add_data():
         tree["columns"] = (headers)
         for header in headers:
             tree.column(header, width=50, minwidth=150, anchor=tk.CENTER)
-        #Create headings
+        # show headers
         for header in headers:
             tree.heading(header, text=header, anchor=tk.CENTER)
         i = 0
+        # show rows
         for row in cursor:
             tree.insert('', "end", text="", values = row)
             i = i + 1
@@ -143,14 +149,12 @@ def add_data():
         hsb = ttk.Scrollbar(r, orient="horizontal")
         hsb.configure(command=tree.xview)
         tree.configure(xscrollcommand=hsb.set)
-        #hsb.pack(fill=X, side = BOTTOM)
         hsb.place(x=75, y=290, width = 898)
 
         #Vertical scrollbar
         vsb = ttk.Scrollbar(r, orient="vertical")
         vsb.configure(command=tree.yview)
         tree.configure(yscrollcommand=vsb.set)
-        #vsb.pack(fill=Y, side = RIGHT)
         vsb.place(x=980, y=3, height=270)
         
             
@@ -160,6 +164,7 @@ def add_data():
             entry = Entry(r, width=15)
             entry.place(x=520, y = yas )
             label = Label(r, text=header, width=12, font=('Times', 11, 'bold'))
+            # instruction for dates
             if (header == "date" or header=="dateOfBirth"):
                 format = Label(r, text="(format: yyyy-mm-dd)", width=15, font=('Times', 11, 'bold'))
                 format.place(x=630, y=yas)
@@ -177,7 +182,7 @@ def add_data():
             cnx.commit()
             datalist.insert(0, cursor.lastrowid)
             tree.insert('', 'end', text="", values=(datalist))
-            #Success message2    
+            #Success message
             mb.showinfo("Sucess", f"{table[:-1]} registered")
             #Empty all entry widgets
             for widget in r.winfo_children():
@@ -198,12 +203,13 @@ def add_data():
     cancelbutton.configure(font=('Times', 11, 'bold'), bg='grey', fg='black')
     cancelbutton.place(x=550, y=630)
 
+
 def get_all_entry(root):
+    # function to get all the entries within a window in a list
     all_entries = [widget for widget in root.winfo_children() if isinstance(widget, Entry)]
     return all_entries
 
 def delete_data(tree):
-
     table = insert.get()
     selected_item=tree.selection()[0]
     did=tree.item(selected_item)['values'][0]
@@ -233,7 +239,6 @@ tableButton = Button(window, text = "Choose", command = retrieveTables)
 tableButton.place(x=175, y=60) # pos for button
 
 # creating entry to insert firstName to find all information about donors
-
 infolabel = Label(window, text="Enter a person's first  and last name to see their information:")
 
 infolabel.place(x=47, y=100)
@@ -245,7 +250,6 @@ infoButton = Button(window, text = "Choose", command = lambda: ('<<openNewWindow
 infoButton.place(x=175, y=160)
 
 # create entry to find when donors can donate next
-
 nextdonlabel = Label(window, text="Enter a donor's first  and last name to know when they can donate again:")
 
 nextdonlabel.place(x=2, y=200)
@@ -257,7 +261,7 @@ nextdonButton = Button(window, text = "Choose", command = lambda: ('<<openNewWin
 nextdonButton.place(x=175, y=260)
 
 
-# create entry for gi
+# create entry for information on whom can  give blood to whom
 labelgiving = Label(window, text="Enter first and last name of a recipient to see who they can get blood from:")
 
 labelgiving.place(x=2, y=300)
@@ -277,6 +281,3 @@ insert.place(x=105, y=400) # pos of combobox
 
 manupbutton = tk.Button(window, text="insert", command=lambda: add_data())
 manupbutton.place(x=175, y=430)
-
-
-window.mainloop()
--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 # bloodbank_database
-
-
-to do:
-
->E/R diagram for report, maybe relational schema?
->Queries
->Interface
->video
->report
